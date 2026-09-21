a04ca00ec25d420696f47c4a635dcacc52a61e99
Anastas Dolushanov <adolushanov@gmail.com>
2026-09-22 02:43:20 +0300

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Gallery } from './components/gallery/gallery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Gallery }, 
+];
